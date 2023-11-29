78a47f6a9ab3825013d17205ce2d66aea669299f
Dani Sternthal <dstrnthl97@gmail.com>
2023-11-29 18:53:13 -0500

    save

---
+++ public/js/comment.js
@@ -0,0 +1,7 @@
+// const addCommentBtn = document.querySelector("#commentBtn");
+
+// const postComment = async (event) => {
+//     console.log("Clicked on Add Comment")
+// }
+
+// addCommentBtn.addEventListener("click", postComment)
